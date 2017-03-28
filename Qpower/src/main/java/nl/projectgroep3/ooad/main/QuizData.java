c7106427c45180ce1b4e99891e6cbd5267e437f1
Dion Rats <dionrats@gmail.com>
2017-03-28 14:40:11 +0200

    how shit

--- Qpower/src/main/java/nl/projectgroep3/ooad/main/QuizData.java
+++ Qpower/src/main/java/nl/projectgroep3/ooad/main/QuizData.java
@@ -3,7 +3,7 @@ package nl.projectgroep3.ooad.main;
 import java.util.ArrayList;
 
 /**
- * Created by Tom van Grinsven on 3/28/2017.
+ * Created by Tom van Grinsven & Ya Boy Dion, Like wadduup on 3/28/2017.
  */
 public class QuizData implements IPersistence {
     public ArrayList<Vraag> getQuestions() {
