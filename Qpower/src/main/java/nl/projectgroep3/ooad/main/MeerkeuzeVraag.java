e77dac7a1d594c0ee4ff5c876ee4455619da8406
Dion Rats <dionrats@gmail.com>
2017-03-28 14:42:00 +0200

    refactor vraag classes

--- Qpower/src/main/java/nl/projectgroep3/ooad/main/MeerkeuzeVraag.java
+++ Qpower/src/main/java/nl/projectgroep3/ooad/main/MeerkeuzeVraag.java
@@ -4,11 +4,14 @@ package nl.projectgroep3.ooad.main;
  * Created by Tom van Grinsven on 3/28/2017.
  */
 public class MeerkeuzeVraag extends Vraag {
+
+    private static final int TIME = 60;
+
     private String[] antwoorden;
 
 
     public MeerkeuzeVraag(String vraag, String[] antwoorden) {
-        super(vraag, 60);
+        super(vraag, TIME);
         this.antwoorden = antwoorden;
     }
 }
--- Qpower/src/main/java/nl/projectgroep3/ooad/main/OpenVraag.java
+++ Qpower/src/main/java/nl/projectgroep3/ooad/main/OpenVraag.java
@@ -4,9 +4,12 @@ package nl.projectgroep3.ooad.main;
  * Created by Tom van Grinsven on 3/28/2017.
  */
 public class OpenVraag extends Vraag {
+
+    private static final int TIME = 120;
+
     private String[] antwoorden;
     public OpenVraag(String vraag, String[] antwoorden) {
-        super(vraag, 120);
+        super(vraag, TIME);
         this.antwoorden = antwoorden;
     }
 }
